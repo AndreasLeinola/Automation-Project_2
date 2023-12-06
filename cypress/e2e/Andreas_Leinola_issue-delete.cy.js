7a0f436fff5a01e4760fae56322c334ded1a5d20
AndreasLeinola <leinolaandreas@gmail.com>
2023-12-06 11:15:00 +0200

    first commit

---
+++ cypress/e2e/Andreas_Leinola_issue-delete.cy.js
@@ -0,0 +1,48 @@
+describe('Delete issue', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
+      //System will already open issue creating modal in beforeEach block  
+      cy.visit(url + '/board?modal-issue=true');
+      });
+    });
+  
+    it('Should delete issue successfully', () => {
+      //Open issue
+      cy.get('[data-testid="list-issue"]')
+      .contains('Try leaving a comment on this issue.')
+      .click()
+
+      //Delete issue
+      cy.get('[data-testid="icon:trash"]').click()
+      cy.contains('Delete issue').click()
+
+      // Assert that the deletion confirmation dialogue is not visible
+      cy.get('.deletion-confirmation-dialogue').should('not.exist')
+
+      //Assert that the issue is deleted and no longer displayed on the Jira board
+      cy.reload()
+      cy.get('[data-testid="board-list:done"]').should('be.empty')
+
+      
+    });
+
+    it('Should cancel deletion process successfully', () => {
+        //Open issue
+      cy.get('[data-testid="list-issue"]')
+      .contains('Try leaving a comment on this issue.')
+      .click()
+
+      //Cancel deletion process
+      cy.get('[data-testid="icon:trash"]').click()
+      cy.contains('Cancel').click()
+
+      //Assert that the deletion confirmation dialogue is not visible
+      cy.get('.deletion-confirmation-dialogue').should('not.exist')
+
+      //Assert that the issue is not deleted and is still displayed on the Jira board
+      cy.reload()
+      cy.get('[data-testid="board-list:done"]').contains('Try leaving a comment on this issue.').should('be.visible')
+
+    })
+});
